982b16772104944927f3fcdf162e35992662a2fe
Vasiliy <SV171094@ya.ru>
2017-02-22 23:48:26 +0200

    Added strict and relaxed entry availability check (issue #2)

--- src/main/java/com/example/models/InventoryRepositoryImpl.java
+++ src/main/java/com/example/models/InventoryRepositoryImpl.java
@@ -24,4 +24,35 @@ public class InventoryRepositoryImpl implements CustomInventoryRepository {
                 "select distinct p from PlantInventoryItem p, PlantReservation r where (r.plant.id = p.id and r.schedule.startDate < sysdate-30*6 and r.schedule.endDate < sysdate-30*6) or (p.id not in (select q.plant.id from PlantReservation q))")
                 .getResultList();
     }
+
+    public boolean isAPlantAvailableStrict(PlantInventoryEntry entry, BusinessPeriod period) {
+        Long count = (Long) em.createQuery(
+                "select count(pi) from PlantInventoryItem pi where (pi.plantInfo.id = ?1 and pi.equipmentCondition = com.example.models.EquipmentCondition.SERVICEABLE and pi.id not in (select pr.plant.id from PlantReservation pr where pr.schedule.startDate < ?3 and pr.schedule.endDate > ?2))")
+                .setParameter(1, entry.id)
+                .setParameter(2, period.getStartDate())
+                .setParameter(3, period.getEndDate())
+                .getSingleResult();
+
+        return count > 0;
+    }
+
+    public boolean isAPlantAvailableRelaxed(PlantInventoryEntry entry, BusinessPeriod period) {
+        boolean b = isAPlantAvailableStrict(entry, period);
+        if (b)
+            return b;
+
+        if (LocalDate.now().isAfter(period.getStartDate().minusWeeks(3)))
+            return false;
+
+        long count = (Long) em.createQuery(
+                "select count(pi) from PlantInventoryItem pi where (pi.plantInfo.id = ?1 and pi.id not in (select pr.plant.id from PlantReservation pr where (pr.schedule.startDate < ?3 and pr.schedule.endDate > ?2)) and pi.id in (select mt.reservation.plant.id from MaintenanceTask mt where mt.reservation.schedule.endDate < ?4 and mt.reservation.schedule.endDate > ?5))")
+                .setParameter(1, entry.id)
+                .setParameter(2, period.getStartDate())
+                .setParameter(3, period.getEndDate())
+                .setParameter(4, period.getStartDate().minusWeeks(1))
+                .setParameter(5, LocalDate.now())
+                .getSingleResult();
+
+        return count > 0;
+    }
 }
--- src/test/java/com/example/models/InventoryRepositoryTests.java
+++ src/test/java/com/example/models/InventoryRepositoryTests.java
@@ -8,6 +8,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.time.LocalDate;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -20,14 +23,166 @@ public class InventoryRepositoryTests {
     @Autowired
     PlantInventoryEntryRepository plantInventoryEntryRepo;
     @Autowired
-    PlantInventoryItemRepository plantInventorItemRepo;
+    PlantInventoryItemRepository plantInventoryItemRepo;
     @Autowired
     PlantReservationRepository plantReservationRepo;
     @Autowired
     InventoryRepository inventoryRepo;
+    @Autowired
+    InventoryRepositoryImpl inventoryRepoImpl;
+    @Autowired
+    MaintenanceTaskRepository maintenanceTaskRepo;
 
     @Test
     public void queryUnhiredPlants() {
         assertThat(inventoryRepo.findUnhired().size()).isEqualTo(2);
     }
+
+    @Test
+    public void checkIfPlantAvailableStrict()
+    {
+        boolean a = inventoryRepoImpl.isAPlantAvailableStrict(
+                plantInventoryEntryRepo.findOne(1L),
+                            BusinessPeriod.of(LocalDate.of(2012, 12, 12), LocalDate.of(2012, 12, 13)));
+        assert(a);
+    }
+
+    private void addReservationAndCheckAvailability(BusinessPeriod of) {
+        PlantReservation reserve = new PlantReservation();
+        reserve.setSchedule(of);
+        reserve.setPlant(plantInventoryItemRepo.findOne(1L));
+        plantReservationRepo.save(reserve);
+    }
+
+    private boolean IsAPlantAvailableStrict() {
+        return inventoryRepoImpl.isAPlantAvailableStrict(
+                plantInventoryEntryRepo.findOne(1L),
+                BusinessPeriod.of(LocalDate.of(2012, 12, 12), LocalDate.of(2012, 12, 22)));
+    }
+
+    private boolean IsAPlantAvailableRelaxed() {
+        return inventoryRepoImpl.isAPlantAvailableRelaxed(
+                plantInventoryEntryRepo.findOne(1L),
+                BusinessPeriod.of(LocalDate.of(2012, 12, 12), LocalDate.of(2012, 12, 22)));
+    }
+
+    @Test
+    public void checkIfPlantAvailableDateNearReservation()
+    {
+        addReservationAndCheckAvailability(BusinessPeriod.of(LocalDate.of(2012, 12, 10), LocalDate.of(2012, 12, 12)));
+        assert(IsAPlantAvailableStrict());
+        assert(IsAPlantAvailableRelaxed());
+    }
+
+    @Test
+    public void checkIfPlantUnavailableStartDateIntersectsWithReservation()
+    {
+        addReservationAndCheckAvailability(BusinessPeriod.of(LocalDate.of(2012, 12, 10), LocalDate.of(2012, 12, 13)));
+        assert(!IsAPlantAvailableStrict());
+        assert(!IsAPlantAvailableRelaxed());
+    }
+
+    @Test
+    public void checkIfPlantUnavailableEndDateIntersectsWithReservation()
+    {
+        addReservationAndCheckAvailability(BusinessPeriod.of(LocalDate.of(2012, 12, 16), LocalDate.of(2012, 12, 25)));
+        assert(!IsAPlantAvailableStrict());
+        assert(!IsAPlantAvailableRelaxed());
+    }
+
+    @Test
+    public void checkIfPlantUnavailableDatesWithinReservation()
+    {
+        addReservationAndCheckAvailability(BusinessPeriod.of(LocalDate.of(2012, 12, 10), LocalDate.of(2012, 12, 25)));
+        assert(!IsAPlantAvailableStrict());
+        assert(!IsAPlantAvailableRelaxed());
+    }
+
+    @Test
+    public void checkIfPlantUnavailableEndDatesOverReservation()
+    {
+        addReservationAndCheckAvailability(BusinessPeriod.of(LocalDate.of(2012, 12, 13), LocalDate.of(2012, 12, 16)));
+        assert(!IsAPlantAvailableStrict());
+        assert(!IsAPlantAvailableRelaxed());
+    }
+
+    @Test
+    public void checkIfPlantAvailableRelaxed()
+    {
+        boolean a = inventoryRepoImpl.isAPlantAvailableRelaxed(
+                plantInventoryEntryRepo.findOne(1L),
+                BusinessPeriod.of(LocalDate.of(2012, 12, 12), LocalDate.of(2012, 12, 13)));
+        assert(a);
+    }
+
+    @Test
+    public void checkIfPlantAvailableUnserviceable()
+    {
+        PlantInventoryItem item = plantInventoryItemRepo.findOne(1L);
+        item.setEquipmentCondition(EquipmentCondition.UNSERVICEABLEREPAIRABLE);
+        plantInventoryItemRepo.save(item);
+        assert(!IsAPlantAvailableStrict());
+        assert(!IsAPlantAvailableRelaxed());
+    }
+
+    private PlantReservation addReservation(PlantInventoryItem item, MaintenanceTask mt) {
+        PlantReservation reservation = new PlantReservation();
+        reservation.setSchedule(mt.getPeriod());
+        reservation.setPlant(item);
+        plantReservationRepo.save(reservation);
+        return reservation;
+    }
+
+    @Test
+    public void checkIfPlantAvailableUnserviceableRelaxed()
+    {
+        LocalDate weekFromNow = LocalDate.now().plusWeeks(1);
+        LocalDate tenDaysFromNow = LocalDate.now().plusDays(10);
+        LocalDate monthFromNow = LocalDate.now().plusMonths(1);
+        LocalDate twoMonthFromNow = LocalDate.now().plusMonths(2);
+
+        maintenanceTaskRepo.deleteAll();
+        plantReservationRepo.deleteAll();
+
+        PlantInventoryItem item = plantInventoryItemRepo.findOne(1L);
+        item.setEquipmentCondition(EquipmentCondition.UNSERVICEABLEREPAIRABLE);
+        plantInventoryItemRepo.save(item);
+
+        MaintenanceTask mt = new MaintenanceTask();
+        mt.setPeriod(BusinessPeriod.of(weekFromNow, tenDaysFromNow));
+        mt.setReservation(addReservation(item, mt));
+        maintenanceTaskRepo.save(mt);
+
+        boolean a = inventoryRepoImpl.isAPlantAvailableRelaxed(
+                plantInventoryEntryRepo.findOne(1L),
+                BusinessPeriod.of(monthFromNow, twoMonthFromNow));
+        assert(a);
+    }
+
+    @Test
+    public void checkIfPlantUnavailableUnserviceableRelaxedLateMaintainance()
+    {
+        LocalDate weekFromNow = LocalDate.now().plusWeeks(1);
+        LocalDate almostMonthFromNow = LocalDate.now().plusDays(27);
+        LocalDate monthFromNow = LocalDate.now().plusMonths(1);
+        LocalDate twoMonthFromNow = LocalDate.now().plusMonths(2);
+
+        maintenanceTaskRepo.deleteAll();
+        plantReservationRepo.deleteAll();
+
+        PlantInventoryItem item = plantInventoryItemRepo.findOne(1L);
+        item.setEquipmentCondition(EquipmentCondition.UNSERVICEABLEREPAIRABLE);
+        plantInventoryItemRepo.save(item);
+
+        MaintenanceTask mt = new MaintenanceTask();
+        mt.setPeriod(BusinessPeriod.of(weekFromNow, almostMonthFromNow));
+        mt.setReservation(addReservation(item, mt));
+        maintenanceTaskRepo.save(mt);
+
+        boolean a = inventoryRepoImpl.isAPlantAvailableRelaxed(
+                plantInventoryEntryRepo.findOne(1L),
+                BusinessPeriod.of(monthFromNow, twoMonthFromNow));
+        assert(!a);
+    }
 }
+
