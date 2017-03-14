e7cc71aac48fc5529bd2aa6a81393e02152e14bd
Stepan Bolotnikov <stepan@voog.com>
2017-03-14 20:22:33 +0200

    Fixed exceptions package name

---
+++ src/main/java/com/example/common/application/exceptions/PlantNotAvailableException.java
@@ -0,0 +1,10 @@
+package com.example.common.application.exceptions;
+
+/**
+ * Created by stepan on 13/03/2017.
+ */
+public class PlantNotAvailableException extends Throwable {
+    public PlantNotAvailableException(String msg) {
+        super(msg);
+    }
+}
---
+++ src/main/java/com/example/common/application/exceptions/PurchaseOrderNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.common.application.exceptions;
+
+/**
+ * Created by stepan on 13/03/2017.
+ */
+public class PurchaseOrderNotFoundException extends Throwable {
+    public PurchaseOrderNotFoundException(String msg) { super(msg); }
+}
